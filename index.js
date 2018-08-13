9d9ce329fe764c5a5def839568da804a428c0318
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-13 15:14:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,3 +21,18 @@ function nowServing(katzDeli) {
     return (`Currently serving ${katzDeli.shift()}.`)
   }
 };
+
+var katzDeliLine = [];
+
+function currentLine(katzDeli) {
+  let i = 0;
+  while (i < katzDeli.length) {
+    katzDeliLine.push(` ` + [i + 1] + `. ` + katzDeli[i])
+    i++;
+  }
+  if (katzDeli.length === 0) {
+    return "The line is currently empty.";
+  } else {
+  return "The line is currently:" +katzDeliLine;
+  }
+}
