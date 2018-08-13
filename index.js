97c87322ed65430b8b68ba78bc50676697189985
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-13 15:14:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,6 +33,6 @@ function currentLine(katzDeli) {
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else {
-  return "The line is currently:" +katzDeliLine;
+    return "The line is currently:" + katzDeliLine;
   }
 }
