318fadf3e4136a374238e7dcb2e926bc87c5b251
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-08-13 14:54:11 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,7 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, customerName) {
+  if (katzDeli.length > 0);
+  katzDeli.push(customerName);
+  return (`Welcome, ${customerName}. You are number ${katzDeli.length} in line.`)
+};
